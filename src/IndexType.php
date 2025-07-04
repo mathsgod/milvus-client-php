6964261c900c80017788cffa643c79bd9da0c921
Raymond Chong <mathsgod@yahoo.com>
2025-07-04 16:45:55 +0800

    ✨ feat(IndexType): 更新索引類型常數以包含新類型
    * 新增了多個索引類型常數以擴展功能。
    * 包括`BITMAP`、`STL_SORT`、`HNSW`、`DISKANN`等，以支援更多的索引選項。
    * 確保與Milvus 2.6.0及以後版本的兼容性。

--- src/IndexType.php
+++ src/IndexType.php
@@ -5,8 +5,28 @@ namespace Milvus;
 class IndexType
 {
     const AUTOINDEX = 'AUTOINDEX';
+
+    const BITMAP = 'BITMAP';
     const INVERTED = 'INVERTED';
-    
+    const STL_SORT = 'STL_SORT';
+
+    const FLAT = 'FLAT';
+    const IVF_FLAT = 'IVF_FLAT';
+    const IVF_PQ = 'IVF_PQ';
+    const IVF_SQ8 = 'IVF_SQ8';
+    const IVF_RABITQ  = 'IVF_RABITQ'; //only for Milvus 2.6.0 and later
+
+    const HNSW = 'HNSW';
+    const HNSW_SQ = 'HNSW_SQ';
+    const HNSW_PQ = 'HNSW_PQ';
+    const HNSW_PRQ = 'HNSW_PRQ';
+    const DISKANN = 'DISKANN';
+
+    const BIN_IVF_FLAT = 'BIN_IVF_FLAT';
+    const BIN_FLAT = 'BIN_FLAT';
+
+    const SPARSE_INVERTED_INDEX = 'SPARSE_INVERTED_INDEX';
+
     const GPU_CAGRAPH = 'GPU_CAGRAPH';
     const GPU_IVF_FLAT = 'GPU_IVF_FLAT';
     const GPU_IVF_PQ = 'GPU_IVF_PQ';
